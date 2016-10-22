dc73e3a035b300237b987058843529ea9db8395c
INADA Naoki <songofacandy@gmail.com>
2016-10-22 04:19:55 +0000

    goroutine profile の間隔調整 7s => 13s

--- src/app/stack.go
+++ src/app/stack.go
@@ -9,7 +9,7 @@ import (
 func init() {
 	go func() {
 		prof := pprof.Lookup("goroutine")
-		tick := time.NewTicker(time.Second * 7)
+		tick := time.NewTicker(time.Second * 13)
 		for range tick.C {
 			os.Stderr.WriteString("-----\n")
 			prof.WriteTo(os.Stderr, 1)
